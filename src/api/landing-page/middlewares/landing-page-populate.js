a7eaf63a3ffd546818e2e57b62e171314c3d8d9d
shayan-ataei <shayanataei1380@gmail.com>
2024-01-29 23:13:00 +0330

    first commit

---
+++ src/api/landing-page/middlewares/landing-page-populate.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * `landing-page-populate` middleware
+ */
+const populate = {
+  metadata: {
+    populate: {
+      metaImage: {
+        populate: true,
+        fields: ['name', 'alternativeText', 'url'],
+      },
+    }
+  },
+  blocks: {
+    populate: {
+      link: {
+        populate: true,
+      },
+      image: {
+        fields: ["name", "alternativeText", "url"],
+      },
+      card: {
+        populate: {
+          image: {
+            fields: ["name", "alternativeText", "url"],
+          },
+        }
+      },
+      plan: {
+        populate: ["services", "link"],
+      },
+      form: {
+        populate: ["input", "button" ],
+      },
+    }
+  },
+};
+
+module.exports = (config, { strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    strapi.log.info('In landing-page-populate middleware.');
+    ctx.query = {
+      populate,
+      ...ctx.query,
+    }
+    await next();
+  };
+};
